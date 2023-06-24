785923ff53541a26cc0660739c73e1ef34bd8161
Battistanic28 <n.battista180@gmail.com>
2023-06-24 14:06:22 -0600

    working implementation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 What:
-- Script to tranform an ADMS-12 csv file exported from repeaterbook.com, to the ADMS-10 format required for the Yaesu FT-70 radio.
+- Script to tranform an ADMS-7 csv file exported from repeaterbook.com, to the ADMS-10 format required for the Yaesu FT-70 radio.
 
 How:
 - `cd` into directory
@@ -7,6 +7,6 @@ How:
 - Import your ADMS-12 csv file - named `data.csv`
 - Run `python3 script.py`
 - Retrieve generated file
-- That is all
+- "Makes no promises, expect no miracles"
 
-Format reference - https://docs.google.com/spreadsheets/d/1SwSSeepxddy1IkkQ96qrCpxQg8kWTUNwcPbX0_AlT_U/edit?usp=sharing
+FT-70 Format reference - https://docs.google.com/spreadsheets/d/1SwSSeepxddy1IkkQ96qrCpxQg8kWTUNwcPbX0_AlT_U/edit?usp=sharing
--- script.py
+++ script.py
@@ -7,17 +7,13 @@ adms10_columns = [
     'Tx POWER', 'SKIP', 'AUTO STEP', 'STEP', 'TAG', 'MEMOY MASK', 'ATT', 'S METER SQL', 'BELL', 'HALF DEV', 'CLOCK SHIFT',
     'BANK 1', 'BANK 2', 'BANK 3', 'BANK 4', 'BANK 5','BANK 7', 'BANK 8', 'BANK 9', 'BANK 10', 'BANK 11','BANK 12', 'BANK 13',
     'BANK 14', 'BANK 15', 'BANK 16','BANK 17', 'BANK 18', 'BANK 19', 'BANK 20', 'BANK 21', 'BANK 12', 'BANK 23', 'BANK 24',
-    'COMMENT' 'BLANK'
+    'COMMENT', 'BLANK'
     ]
 
-# column_transfer_pattern = [0,'priority channel',1,2,3,4,'auto mode',5,'AMS','Dig/Analog',15,7,8,9,
-#                            'DGS Polarity',10,11,12,'auto step',13,
-#                         ]
-
-df_input = pd.read_csv('data.csv', on_bad_lines='skip', header=None)
+df_input = pd.read_csv('data2.csv', on_bad_lines='skip', header=None)
 df_output = pd.DataFrame();
 
-columns_to_transfer = [0,1,2,3,4,5,15,7,8,9,10,11,12,13]
+columns_to_transfer = [0,1,2,3,4,5,15,7,8,9,10,11,12,13,16]
 
 # Clean channel description data
 df_input[15] = df_input[15].apply(lambda x: x.split(' - ', 1)[-1])
@@ -32,6 +28,16 @@ df_output.insert(8, 'AMS', 'ON');
 df_output.insert(9, 'DIG/ANALOG', 'ANALOG');
 df_output.insert(14, 'DCS POLARITY', 'RX Normal TX Normal');
 df_output.insert(18, 'AUTO STEP', 'ON');
+df_output.insert(20, 'COMMENT', '');
+
+
+# Set defaults for remaining configs and banks
+for idx, column in enumerate(adms10_columns[20: 50]):
+    # TODO: Remove hardcoding
+    push_column = 20 + idx
+
+    df_output.insert(push_column, 'BANK'+str(idx), 'OFF')
+
 
 # Rename columns
 column_dict = dict(zip_longest(df_output.columns, adms10_columns))
@@ -39,3 +45,7 @@ renamed_df_output = df_output.rename(column_dict, axis='columns')
 
 # Print output
 print(renamed_df_output)
+
+# Export to csv
+renamed_df_output.to_csv('output.csv', index=False)
+
